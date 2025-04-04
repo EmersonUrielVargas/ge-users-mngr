548c879a1bf3b29652170fd110943d3bd78c9b15
EmersonUrielVargas <emrvargaitaz@gmail.com>
2025-04-04 01:03:29 -0500

    fix error handler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,5 +1,6 @@
 import { Request, NextFunction, Response } from "express";
 import { constants } from "http2";
+import { QueryFailedError } from "typeorm";
 
 export const errorHandler = (err: any, _req:Request, response: Response, _next: NextFunction)=>{
     let customErr = err;
@@ -8,9 +9,13 @@ export const errorHandler = (err: any, _req:Request, response: Response, _next: 
     } catch{}
     if (customErr?.description) {
         response.status(customErr.statusCode).send(customErr);
+    }else{
+        if (err instanceof QueryFailedError) {
+            customErr.message = 'Error saving data in BD';
+        }
+        response.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({
+            statusCode: constants.HTTP_STATUS_INTERNAL_SERVER_ERROR,
+            description: err.message
+        })
     }
-    response.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({
-        statusCode: constants.HTTP_STATUS_INTERNAL_SERVER_ERROR,
-        description: err.message
-    })
 }
